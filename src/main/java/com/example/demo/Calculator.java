1a6b44ad37a28621809073720fe8d813e7f78066
Rabiya Dar <rabiya_khalid@hotmail.com>
2025-11-05 14:18:02 +0100

    First Commit

---
+++ src/main/java/com/example/demo/Calculator.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+public class Calculator {
+
+    public int add(int a , int b){
+        return a+b;
+    }
+
+    public int subtract(int a , int b){
+        return a-b;
+    }
+    public int division(int a , int b){
+        if (b==0){
+            throw new IllegalArgumentException("Division by zero impossible");
+        }
+        else
+            return a/b;
+    }
+    public int multiply(int a , int b){
+        return a * b;
+
+    }
+    public int square(int a ){
+        return a * a;
+
+    }
+
+    public double power(double a, double b) {
+
+        return Math.pow(a,b);
+    }
+}
